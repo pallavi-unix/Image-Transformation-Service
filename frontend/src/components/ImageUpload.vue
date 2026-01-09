d358d379d254e60c4a005266ccae8e8894fa6558
Pallavi Deshmukh <pallavideshmukh32897@gmail.com>
2026-01-09 00:05:25 -0500

    update: upgrade in the UI

--- frontend/src/components/ImageUpload.vue
+++ frontend/src/components/ImageUpload.vue
@@ -85,8 +85,9 @@ export default defineComponent({
     // File selection handler
     const onFileChange = (event: Event) => {
       const input = event.target as HTMLInputElement;
+
       if (input.files && input.files.length > 0) {
-        selectedFile.value = input.files[0];
+        selectedFile.value = input.files[0]!;
       } else {
         selectedFile.value = null;
       }
